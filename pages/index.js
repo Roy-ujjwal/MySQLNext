51d3d7458455f9ddeb26037e02127474dee01b22
Roy-ujjwal <kumarujjwal1102@gmail.com>
2022-10-13 22:53:17 +0530

    edited checkbox

--- pages/index.js
+++ pages/index.js
@@ -81,19 +81,6 @@ export default function Home() {
                 autoComplete="off"
               >
                 <Form.Item
-                  label="Username"
-                  name="username"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your username!",
-                    },
-                  ]}
-                >
-                  <Input />
-                </Form.Item>
-
-                <Form.Item
                   label="Password"
                   name="password"
                   rules={[
